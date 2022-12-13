d3c70075e72a5137227a7a80e443ffb294ac30de
DenSenY <95503241+DenSenY@users.noreply.github.com>
2022-12-13 12:20:19 +0100

    Update .sh

--- .sh
+++ .sh
@@ -1,10 +1,16 @@
 echo " dialog --inputbox " Bitte username eingeben! "
 username = $(dialog --inputbox " Bitte username eingeben! "
 echo " dialog --inputbox " Sind sie sicher?"  "
-YESNO = $dialog --inputbox " Sind sie sicher? "
+YESNO = $(dialog --inputbox " Sind sie sicher?) "
 
 until [ "$YESNO" = "1" ]
   do
-		echo " dialog --inputbox " Bitte username eingeben! "
+	echo " dialog --inputbox " Bitte username eingeben! "
 		
-		YESNO= 0
+	YESNO = 0
+	clear 
+done
+
+
+
+
